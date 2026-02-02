c5bdd9b10c4a4e9b124560a00d7d539ee85b4d84
shufanshahi <shufanshahi@gmail.com>
2026-02-02 15:30:28 +0600

    _edge_index_to_adjacency_matrix and _heter_no_weight_edge written

--- module.py
+++ module.py
@@ -8,6 +8,74 @@ from torch.nn import Parameter
 def _get_clones(module, N):
     return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
 
+
+
+    def _edge_index_to_adjacency_matrix(self,
+                                        edge_index,
+                                        edge_weight=None,
+                                        num_nodes=100,
+                                        no_cuda=False):
+
+        if edge_weight is not None:
+            edge_weight = edge_weight.squeeze()
+        else:
+            edge_weight = torch.ones(
+                edge_index.size(1)).cuda() if not no_cuda else torch.ones(
+                    edge_index.size(1))
+        adj_sparse = torch.sparse_coo_tensor(edge_index,
+                                             edge_weight,
+                                             size=(num_nodes, num_nodes))
+        adj = adj_sparse.to_dense()
+        row_sum = torch.sum(adj, dim=1)
+        d_inv_sqrt = torch.pow(row_sum, -0.5)
+        d_inv_sqrt[d_inv_sqrt == float('inf')] = 0
+        d_inv_sqrt_mat = torch.diag_embed(d_inv_sqrt)
+        gcn_fact = torch.matmul(d_inv_sqrt_mat,
+                                torch.matmul(adj, d_inv_sqrt_mat))
+
+        if not no_cuda and torch.cuda.is_available():
+            gcn_fact = gcn_fact.cuda()
+
+        return gcn_fact
+
+
+    def _heter_no_weight_edge(self, feature, num_modal, dia_lens, win_p,
+                              win_f):
+        index_inter = []
+        all_dia_len = sum(dia_lens)
+        all_nodes = list(range(all_dia_len * num_modal))
+        nodes_uni = [None] * num_modal
+
+        for m in range(num_modal):
+            nodes_uni[m] = all_nodes[m * all_dia_len:(m + 1) * all_dia_len]
+
+        start = 0
+        for dia_len in dia_lens:
+            for m, n in permutations(range(num_modal), 2):
+
+                for j, node_m in enumerate(nodes_uni[m][start:start +
+                                                        dia_len]):
+                    if win_p == -1 and win_f == -1:
+                        nodes_n = nodes_uni[n][start:start + dia_len]
+                    elif win_p == -1:
+                        nodes_n = nodes_uni[n][
+                            start:min(start + dia_len, start + j + win_f + 1)]
+                    elif win_f == -1:
+                        nodes_n = nodes_uni[n][max(start, start + j -
+                                                   win_p):start + dia_len]
+                    else:
+                        nodes_n = nodes_uni[n][
+                            max(start, start + j -
+                                win_p):min(start + dia_len, start + j + win_f +
+                                           1)]
+                    index_inter.extend(list(product([node_m], nodes_n)))
+            start += dia_len
+        edge_index = (torch.tensor(index_inter).permute(1, 0).cuda() if
+                      not self.no_cuda else torch.tensor(index_inter).permute(
+                          1, 0))
+
+        return edge_index
+
 class HeterGConvLayer(torch.nn.Module):
 
     def __init__(self, feature_size, dropout=0.3, no_cuda=False):
